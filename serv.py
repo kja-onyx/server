861822bce2eed994c1b22190dc3bb3bbcbf16dc8
kja-onyx <aditya@onyxindia.com>
2026-06-13 17:37:11 +0530

    Create serv.py

---
+++ serv.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import time
+ 
+app = Flask(__name__)
+ 
+@app.route("/imu", methods=["POST"])
+def imu():
+    data = request.json
+ 
+    print(
+        time.strftime("%H:%M:%S"),
+        data
+    )
+ 
+    return "OK", 200
+ 
+ 
+@app.route("/", methods=["GET"])
+def home():
+    return "ESP32 IMU Server Running", 200
+ 
+ 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
