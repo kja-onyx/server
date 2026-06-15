13c8f497ba8d8806571280dae889af16e538e08d
kja-onyx <aditya@onyxindia.com>
2026-06-15 10:54:55 +0530

    Update serv.py

--- serv.py
+++ serv.py
@@ -1,25 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import time
  
 app = Flask(__name__)
  
-@app.route("/imu", methods=["POST"])
-def imu():
-    data = request.json
- 
-    print(
-        time.strftime("%H:%M:%S"),
-        data,
-        flush=True
-    )
- 
-    return "OK", 200
- 
+latest_data = {}
  
 @app.route("/", methods=["GET"])
 def home():
-    return "ESP32 IMU Server Running", 200
+    return "ESP32 IMU Server Running"
  
+@app.route("/imu", methods=["POST"])
+def imu():
+    global latest_data
+ 
+    latest_data = request.json
+ 
+    print(time.strftime("%H:%M:%S"), latest_data)
+ 
+    return {"status": "ok"}, 200
+ 
+@app.route("/latest", methods=["GET"])
+def latest():
+    return jsonify(latest_data)
  
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
